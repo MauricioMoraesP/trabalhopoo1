263fac3c188c1897594abc89a51628694c8e057e
Maurício Moraes <mmprogrammer@hotmail.com>
2022-06-21 02:42:38 -0300

    att1

---
+++ src/RU/Cliente.java
@@ -0,0 +1,44 @@
+package RU;
+
+import java.util.Calendar;
+
+public class Cliente {
+
+	private String nome;
+	private int cpf;
+	private int telefone;
+	private Calendar nascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public Calendar getNascimento() {
+		return nascimento;
+	}
+
+	public void setNascimento(Calendar nascimento) {
+		this.nascimento = nascimento;
+	}
+
+}
